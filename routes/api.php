60252f63f3b91d5a7fb44c449f4ddcf2f4e6992a
patrickodey <patrickodey419@gmail.com>
2021-09-23 23:07:25 +0100

    created api to delete individual product

--- routes/api.php
+++ routes/api.php
@@ -25,3 +25,4 @@ Route::get('/products', [ProductController::class, 'products']);
 Route::post('/products', [ProductController::class, 'store']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::put('/products/{id}', [ProductController::class, 'update']);
+Route::delete('/products/{id}', [ProductController::class, 'delete']);
